d64ebe50ca13fbc6388ecd3204b86d9db8366e4a
L7ucky <L7ucky@gmail.com>
2014-04-23 16:03:35 -0600

    Feature added Send emails
    Send emails to the employee for a given amount of time.... Right now its
    set to send to the schedulemeapplication@gmail.com

--- S_ME_SITE/app/routes.js
+++ S_ME_SITE/app/routes.js
@@ -1,6 +1,9 @@
 // app/routes.js
 var mongoose = require('mongoose');
-//var JSON = require('JSON');
+var nodemailer = require('nodemailer');
+var async = require('async');
+
+
 //
 //
 // DEFINE SCHEMAS
@@ -94,7 +97,7 @@ module.exports = function(app, passport) {
 	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get('/calendar', isLoggedIn, function(req, res) {
                 res.render('calendar.ejs', {
-                    user : req.user, // get the user out of session and pass to template
+                    user : req.user // get the user out of session and pass to template
                 });
 	});
 	
@@ -112,7 +115,7 @@ module.exports = function(app, passport) {
 		Shift.find({ "User_email": req.user.google.email }, function(err, shift) {
 			if(!err) {
 				res.json(200, shift);
-				console.log(shift);
+				//console.log(shift);
 			}
 			else
 				console.log(err);
@@ -144,7 +147,7 @@ module.exports = function(app, passport) {
     app.post('/saveShifts', isLoggedIn, function(req, res) {
 
         var newShifts = req.body;
-        console.log(newShifts);
+        //console.log(newShifts);
 
         for(var i = 0; i<newShifts.length; i++){
 
@@ -191,6 +194,131 @@ module.exports = function(app, passport) {
                 successRedirect : '/calendar',
                 failureRedirect : '/'
             }));
+
+    // =====================================
+    // SEND EMAILS =======================
+    // =====================================
+
+
+
+    app.post('/sendShifts', isLoggedIn, function(req, res) {
+        var requestObject= req.body;
+
+
+
+        var startDate = new Date();
+            startDate.setDate(requestObject.startDay);
+            startDate.setMonth(requestObject.startMonth);
+            startDate.setFullYear(requestObject.startYear);
+
+        var endDate = new Date();
+            endDate.setDate(requestObject.endDay);
+            endDate.setMonth(requestObject.endMonth);
+            endDate.setFullYear(requestObject.endYear);
+
+        var schedule = {};// object 1
+        var dates = [];
+        while(startDate.getDate() <= endDate.getDate() || startDate.getMonth() != endDate.getMonth() || startDate.getFullYear() != endDate.getFullYear()){
+
+            var tempDate = new Date();
+                tempDate.setDate(startDate.getDate());
+                tempDate.setMonth(startDate.getMonth());
+                tempDate.setFullYear(startDate.getFullYear());
+            dates.push(tempDate);
+            startDate.setDate(startDate.getDate() +1);
+        }
+
+        // 1st parameter in async.each() is the array of items
+        async.each(dates,
+            // 2nd parameter is the function that each item is passed into
+            function(currDate, callback){
+                // Call an asynchronous function (often a save() to MongoDB)
+                Shift.find({"User_email": req.user.google.email, "date._day":currDate.getDate(), "date.month":currDate.getMonth(), "date.year":currDate.getFullYear()}, function(err, shiftArray) {
+                    callback();
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("...........NEW DAY..........");
+                        for (var i = 0; i < shiftArray.length; i++) {
+                            console.log(shiftArray[i].employee_email + " Has a shift today");
+                            var tempShift = {};// object 2
+
+                            tempShift.employee_email = shiftArray[i].employee_email;
+                            tempShift.User_email = shiftArray[i].User_email;
+                            tempShift._day = shiftArray[i].date._day;
+                            tempShift.month = shiftArray[i].date.month;
+                            tempShift.year = shiftArray[i].date.year;
+                            tempShift.start_time = shiftArray[i].start_time;
+                            tempShift.end_time = shiftArray[i].end_time;
+                            tempShift.shiftStartIndex = shiftArray[i].shiftEndIndex;
+                            tempShift.shiftEndIndex = shiftArray[i].shiftEndIndex;
+                            var shiftText ="";
+                            shiftText += tempShift.month + "/" + tempShift._day + "/" + tempShift.year + "\n";
+                            shiftText += "\t" + tempShift.start_time + " to " + tempShift.end_time + "\n\n";
+                            if(schedule[shiftArray[i].employee_email] == null)
+                                schedule[shiftArray[i].employee_email] = "";
+                            schedule[shiftArray[i].employee_email]+=(shiftText);
+                        }
+                    }
+                });
+            },
+            // 3rd parameter is the function call when everything is done
+            function(err){
+                if(err){
+                    console.log(err);
+
+                }
+                else{
+                    // create reusable transport method (opens pool of SMTP connections)
+                    var smtpTransport = nodemailer.createTransport("SMTP", {
+                        service: "Gmail",
+                        auth:{
+
+                            user: "schedulemeapplication@gmail.com",
+                            pass: "Jordan360Andrew"
+
+//                XOAuth2: {
+//                    user: req.user.google.email,
+//                    clientId: req.user.google.id,
+//                    clientSecret: req.user.google.secret,
+//                    refreshToken: req.user.google.refresh,
+//                    accessToken: req.user.google.token,
+//                    timeout: 3600
+//                }
+                        }
+                    });
+
+                    for(key in schedule) {
+                        if (schedule.hasOwnProperty(key)) {
+                            var bodyText = req.user.google.name + " Scheduled you for:\n\n";
+                            bodyText +=schedule[key];
+
+                            // setup e-mail data with unicode symbols
+                            var mailOptions = {
+                                from: req.user.google.email, // sender address
+                                to: "schedulemeapplication@gmail.com", // list of receivers ************************************CHANGE ME BACK TO KEY!!!!!!!!!!!**********************************88
+                                subject: "Your ScheduleME from: " + requestObject.startMonth + "/" + requestObject.startDay + "/" + requestObject.startYear +
+                                    "to: " + requestObject.endMonth + "/" + requestObject.endDay + "/" + requestObject.endYear, // Subject line
+                                text: bodyText // plaintext body
+                            }
+
+                            // send mail with defined transport object
+                            smtpTransport.sendMail(mailOptions, function (error, response) {
+                                if (error) {
+                                    console.log(error);
+                                } else {
+                                    console.log("Message sent: " + response.message);
+                                }
+                            });
+                        }
+                    }
+                    smtpTransport.close(); // shut down the connection pool, no more messages
+                    res.send(200);
+                }
+            }
+        );
+    });
 };
 
 
